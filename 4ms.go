05ef335ce7ac3f573a2e4d4cf89f457fc40d5699
SunnatilloTilavov <sunnatillo0987@gmail.com>
2024-02-12 19:52:07 +0500

    test

--- 4ms.go
+++ 4ms.go
@@ -6,8 +6,6 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
-	// "path/filepath"
-	// "strconv"
 )
 
 func main() {
@@ -18,32 +16,35 @@ func main() {
 	rec := 0
 	fmt.Println("rec:")
 	fmt.Scan(&rec)
-	a := make([]int, rec)
-	if !checkFileExists(filePath + ".text") {
-		for i := 0;i<=5; i++ {
-			rec2 := filePath + strconv.Itoa(rand.Intn(100)) + ".txt"
-			if checkFileExists(rec2) {
-				a=append(a,rec)
+	a := make([]string, rec)
+	if checkFileExists(filePath + ".txt") {
+		for i := 0; i <= 5; i++ {
+			rec2 := filePath + strconv.Itoa(rand.Intn(100))
+			if checkFileExists(rec2 + ".txt") {
 				fmt.Println("recname", rec2)
-			
-			}else{
-				_, err := os.Create( rec2)
-				if err != nil {
-					panic(err)
-				}
-
-			} 
-			if len(a) == rec {
+				a = append(a, rec2)
+			}
+			if i == rec {
 				break
 
 			}
 		}
-	}else {
-		_, err := os.Create(filePath + ".txt")
+	} else {
+		file1, err := os.Create(filePath + ".txt")
 		if err != nil {
 			panic(err)
 		}
+		defer file1.Close()
 	}
+	filename := ""
+	fmt.Println("faylga nom tanlang")
+	fmt.Scan(&filename)
+	file, err := os.Create(filename + ".txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
 }
 
 func checkFileExists(filePath string) bool {
