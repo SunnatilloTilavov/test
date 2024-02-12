5b4e08cfb87a21062361d994aa542952e3918416
SunnatilloTilavov <sunnatillo0987@gmail.com>
2024-02-12 15:58:11 +0500

    test

--- go.mod
+++
@@ -1,5 +0,0 @@
-module 12321
-
-go 1.18
-
-require github.com/intliang/go_greetings v0.1.1
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-	"github.com/intliang/go_greetings"
-)
-func main(){
-	fmt.Println(greetings.Hello("13123"))
-}
